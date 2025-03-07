649407223b1445c33ec36f24fac01d8cd2ca3007
Akshay Jadhav <akshayj2305@gmail.com>
2025-03-07 15:56:13 +0530

    ipfs

--- SheCurity_Backend/shecurity/urls.py
+++ SheCurity_Backend/shecurity/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for shecurity project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from complaints.views import hello_world,ecommerce_data,upload_to_ipfs
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/hello/', hello_world),
+    path('api/ecommerce/', ecommerce_data),
+    path('api/uploadtoipfs/', upload_to_ipfs),
 ]
